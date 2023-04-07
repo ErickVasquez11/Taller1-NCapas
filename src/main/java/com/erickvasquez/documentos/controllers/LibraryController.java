a4365022153db0298592ce52783c65fdbef59e95
ErickVasquez11 <00065520@uca.edu.sv>
2023-04-07 17:56:46 -0600

    :construction: Start the work

---
+++ src/main/java/com/erickvasquez/documentos/controllers/LibraryController.java
@@ -0,0 +1,12 @@
+package com.erickvasquez.documentos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibraryController {
+	@GetMapping("/login")
+	public String getLogin()	{
+		return "login";
+	}
+}
